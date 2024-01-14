42a139fefc0d82f80ef25307cdfb4d0cd9009d01
oscar.jigbring <oscar.jigbring@hotmail.se>
2024-01-14 21:16:26 +0100

    change positions, centered with better logic

--- src/PauseMenu.ts
+++ src/PauseMenu.ts
@@ -8,34 +8,48 @@ class PauseMenu extends Menu {
 
   private drawDialogBox() {
     push();
-    fill("#bcbcbc");
-    translate(-350, -250);
-    rect(width / 2, height / 2, 702, 553);
+    fill("#D9D9D9");
     rectMode(CENTER);
+    rect(width * 0.5, height * 0.5, 702, 553);
+    pop();
+  }
+
+  private drawGreyBackground() {
+    push();
+    fill(224, 224, 224, 10);
+    rect(0, 0, width, height);
     pop();
   }
   private drawHeading() {
     push();
     textSize(100);
-    text("PAUSED", 700, 400);
+    textAlign(CENTER);
+    text("PAUSED", width * 0.5, height * 0.4);
     fill("black");
     pop();
   }
   private drawButtons() {
     push();
-    fill("red");
-    rect(500, height / 2, 200, 110);
+    fill("white");
+    rectMode(CENTER);
+    rect(width * 0.42, height * 0.5, 250, 110);
+    rect(width * 0.58, height * 0.5, 250, 110);
+    rect(width * 0.5, height * 0.65, 250, 110);
     pop();
   }
   private drawButtonTexts() {
     push();
     fill("black");
     textSize(30);
-    text("RESUME", 530, 550, 60);
+    textAlign(CENTER);
+    text("RESUME", width * 0.42, height * 0.49, 0);
+    text("RESTART", width * 0.58, height * 0.49, 0);
+    text("EXIT", width * 0.5, height * 0.64, 0);
     pop();
   }
 
   public draw() {
+    this.drawGreyBackground();
     this.drawDialogBox();
     this.drawHeading();
     this.drawButtons();
