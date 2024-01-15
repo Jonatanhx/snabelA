6dc9fdc697ec6f412c9a5dc7612ca00a5b1fa414
Michaela <mi-and@outlook.com>
2024-01-15 19:03:26 +0100

    changed rect and text

--- levelPickedMenu.ts
+++ levelPickedMenu.ts
@@ -10,11 +10,11 @@ class LevelPickedMenu {
   constructor() {
     this.button1X = width * 0.42;
     this.button1Y = height * 0.5;
-    this.buttonWidth = width * 0.15;
-    this.buttonHeight = width * 0.065;
+    this.buttonWidth = width * 0.05;
+    this.buttonHeight = width * 0.055;
     this.button1Area = (this.buttonWidth * this.buttonHeight) / 20;
     this.buttonFontSize = width * 0.015;
-    this.headingFontSize = width * 0.06;
+    this.headingFontSize = width * 0.02;
   }
 
   private drawDialogBox() {
@@ -35,7 +35,7 @@ class LevelPickedMenu {
     push();
     textSize(this.headingFontSize);
     textAlign(CENTER);
-    text("PAUSED", width * 0.5, height * 0.4);
+    text("Level picked", width * 0.5, height * 0.4);
     fill("black");
     pop();
   }
@@ -43,9 +43,8 @@ class LevelPickedMenu {
     push();
     fill("white");
     rectMode(CENTER);
-    rect(this.button1X, this.button1Y, this.buttonWidth, this.buttonHeight);
-    rect(width * 0.58, height * 0.5, this.buttonWidth, this.buttonHeight);
-    rect(width * 0.5, height * 0.65, this.buttonWidth, this.buttonHeight);
+    rect(width * 0.45, height * 0.48, this.buttonWidth, this.buttonHeight);
+    rect(width * 0.52, height * 0.48, this.buttonWidth, this.buttonHeight);
     pop();
   }
   private drawButtonTexts() {
@@ -53,9 +52,8 @@ class LevelPickedMenu {
     fill("black");
     textSize(this.buttonFontSize);
     textAlign(CENTER);
-    text("RESUME", width * 0.42, height * 0.49, 0);
-    text("RESTART", width * 0.58, height * 0.49, 0);
-    text("EXIT", width * 0.5, height * 0.64, 0);
+    text("Level 1", width * 0.45, height * 0.45, 0);
+    text("Level 2", width * 0.52, height * 0.45, 0);
     pop();
   }
 
