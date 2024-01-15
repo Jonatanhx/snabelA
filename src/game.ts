0140ac4cc88b04aa550a123e2de233c601ea466d
Michaela <mi-and@outlook.com>
2024-01-15 18:57:16 +0100

    add levelpickedmenu to game

--- src/game.ts
+++ src/game.ts
@@ -1,9 +1,11 @@
 class Game {
+  levelPickedMenu = new LevelPickedMenu();
   constructor() {}
 
   public update() {}
 
   public draw() {
     background("black");
+    this.levelPickedMenu.draw();
   }
 }
