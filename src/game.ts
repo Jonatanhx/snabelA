12f643ae4d4dbc1c3fd9cdd70553859b0b34e70c
Philip Bendiksen <philipbendiksen@gmail.com>
2024-01-16 09:41:48 +0100

    wdwad

--- src/game.ts
+++ src/game.ts
@@ -1,27 +1,27 @@
 class Game {
   //Lokala variabel och typ definitioner. Game ska innehålla en meny, en level och en fabrik för att skapa nya levlar.
-  private activeMenu: Menu;
+  /*   private activeMenu: Menu; */
   private level: Level;
   private levelFactory: LevelFactory;
 
   // Här skapar vi instanserna av det vi har definierat.
   constructor() {
     //Vad ska finnas i början?
-    this.activeMenu = new Menu();
+    /*  this.activeMenu = new Menu(); */
     this.level = new Level();
     this.levelFactory = new LevelFactory();
   }
 
   // ------------ METODER ------------------
   //
-  public nextLevel() {}
+  public nextLevel() { }
 
-  private muteMain() {}
+  private muteMain() { }
 
-  private muteSfx() {}
+  private muteSfx() { }
 
   //räknar ut ändringar som ska ske i instanser av game
-  public update() {}
+  public update() { }
 
   public draw() {
     background("black");
