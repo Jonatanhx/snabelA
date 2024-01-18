de29b8ddcf7eeb8e36451f2dacb27ff691f2d521
sararamouz <sara.ram85@gmail.com>
2024-01-18 22:41:06 +0100

    edit

--- src/gameOver.ts
+++ src/gameOver.ts
@@ -6,13 +6,6 @@ class GameOverMenu implements IMenu {
     this.countdown = 3;
     this.gameOverText = "You Died";
   }
-  private tickCountDown() {
-    // For simplicity, decrement countdown for demonstration purposes
-    //is checking whether the current frame count is a multiple of 60
-    if (frameCount % 60 === 0 && this.countdown > 0) {
-      this.countdown--;
-    }
-  }
   private drawHeading() {
     let redColor = color(191, 32, 38, 127); // Red color with 50% opacity (127 out of 255)
     background(redColor);
@@ -24,6 +17,14 @@ class GameOverMenu implements IMenu {
   private drawCountdown() {
     textSize(30);
     text("Restarting in " + this.countdown + "...", width / 2, height / 2 + 50);
+  }
+
+  private tickCountDown() {
+    // For simplicity, decrement countdown for demonstration purposes
+    //is checking whether the current frame count is a multiple of 60
+    if (frameCount % 60 === 0 && this.countdown > 0) {
+      this.countdown--;
+    }
   }
 
   public draw(): void {
