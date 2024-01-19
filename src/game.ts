68941fb86ae71850d59aec0c703ef22eb53d7421
sararamouz <sara.ram85@gmail.com>
2024-01-19 11:44:53 +0100

    add gameover menu

--- src/game.ts
+++ src/game.ts
@@ -1,36 +1,56 @@
 class Game {
+  private activeMenu: IMenu;
   //Lokala variabel och typ definitioner. Game ska innehålla en meny, en level och en fabrik för att skapa nya levlar.
-  /*   private activeMenu: Menu; */
+  /* private activeMenu: Menu; */
+  private activeMenu: IMenu; //Polymorfism
   private level: Level;
   private levelFactory: LevelFactory;
-  private gameOver: GameOverMenu;
 
   // Här skapar vi instanserna av det vi har definierat.
   constructor() {
     //Vad ska finnas i början?
-    /*  this.activeMenu = new Menu(); */
     this.level = new Level();
     this.levelFactory = new LevelFactory();
-    this.gameOver = new GameOverMenu();
+    this.activeMenu = new StartMenu();
   }
 
-  // ------------ METODER ------------------
-  //
   public nextLevel() {}
 
   private muteMain() {}
 
   private muteSfx() {}
 
-  //räknar ut ändringar som ska ske i instanser av game
+  public setActiveMenu(menu: IMenu) {
+    this.activeMenu = menu;
+  }
+
   public update() {
-    if (this.gameOver) {
-      this.gameOver.update();
-    } else {
-      // Game update logic ….
+    if (keyIsDown(RIGHT_ARROW)) {
+      if (this.activeMenu instanceof StartMenu) {
+        this.activeMenu = new GameOverMenu();
+      } else if (this.activeMenu instanceof GameOverMenu) {
+        this.activeMenu = new PauseMenu();
+      } else if (this.activeMenu instanceof PauseMenu) {
+        this.activeMenu = new LevelPickerMenu();
+      } else if (this.activeMenu instanceof LevelPickerMenu) {
+        this.activeMenu = new StartMenu();
+      }
+    } else if (keyIsDown(LEFT_ARROW)) {
+      if (this.activeMenu instanceof StartMenu) {
+        this.activeMenu = new LevelPickerMenu();
+      } else if (this.activeMenu instanceof GameOverMenu) {
+        this.activeMenu = new StartMenu();
+      } else if (this.activeMenu instanceof PauseMenu) {
+        this.activeMenu = new GameOverMenu();
+      } else if (this.activeMenu instanceof LevelPickerMenu) {
+        this.activeMenu = new PauseMenu();
+      }
     }
   }
+
   public draw() {
-    this.gameOver.draw();
+    background("white");
+    this.level.draw(); // Rita ut level
+    this.activeMenu.draw();
   }
 }
