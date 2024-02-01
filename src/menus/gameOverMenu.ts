47f9f10cc838ab7ef7bb70a207908723bd378b8f
sararamouz <sara.ram85@gmail.com>
2024-02-01 20:52:33 +0100

    add comments

--- src/menus/gameOverMenu.ts
+++ src/menus/gameOverMenu.ts
@@ -6,19 +6,21 @@ class GameOverMenu implements IMenu {
   private game: CurrentActiveMenu;
 
   constructor(game: CurrentActiveMenu) {
+    // Initialize properties
     this.countdown = 3;
     this.game = game;
     this.headingWidth = width * 0.5;
     this.headingHeight = height * 0.6;
     this.headingFontSize = width * 0.04;
   }
+  // Draw a red semi-transparent background
   private drawHeading() {
     push();
     let redColor = color(191, 32, 38, 127);
     background(redColor);
     pop();
   }
-
+  // Draw the "GAME OVER" heading
   private drawGameOverHeading() {
     push();
     textSize(this.headingFontSize);
@@ -31,7 +33,7 @@ class GameOverMenu implements IMenu {
     fill("black");
     pop();
   }
-
+  // Draw the "You Died" text
   private drawGameOverText() {
     push();
     textAlign(CENTER, CENTER);
@@ -40,7 +42,7 @@ class GameOverMenu implements IMenu {
     text("You Died", width / 2, height / 2);
     pop();
   }
-
+  // Draw the countdown and restart message
   private drawCountdown() {
     push();
     textSize(30);
@@ -57,6 +59,7 @@ class GameOverMenu implements IMenu {
     strokeWeight(3);
     // textFont(font.gameFont);
     text("Restarting", width * 0.47, height * 0.5 + 50);
+    // Set text color based on countdown value
     if (this.countdown === 3) {
       fill(255, 0, 0);
       textSize(50);
@@ -73,35 +76,32 @@ class GameOverMenu implements IMenu {
       stroke("black");
       strokeWeight(3);
     }
+    // Display countdown text
     text(this.countdown, width * 0.55, height * 0.498 + 50);
     /* text("Restarting in " + this.countdown + "...", width / 2, height / 2 + 50); */
     pop();
   }
-
+  // Update countdown and restart level if countdown reaches 1
   private tickCountDown() {
-    // For simplicity, decrement countdown for demonstration purposes
-    //is checking whether the current frame count is a multiple of 60
     if (frameCount % 30 === 0) {
       if (this.countdown > 1) {
         this.countdown--;
       } else if (this.countdown === 1) {
-        /* setTimeout(() => { */ // tas bort för att skipa fördröjningen
-        // ÄNDRA DETTA, VI SKALL INTE ANVÄNDA GLOBALA VARIABLER
+        // Restart level when countdown reaches 1
         this.game.restartLevel();
         this.game.setActiveMenu(undefined);
         console.log("restarting elvel");
-        /*   }, 1000); */
       }
     }
   }
-
+  // Public method to draw elements on the screen
   public draw(): void {
     this.drawHeading();
     this.drawGameOverText();
     this.drawCountdown();
     this.drawGameOverHeading();
   }
-
+  // Public method to update the countdown
   public update(): void {
     this.tickCountDown();
   }
